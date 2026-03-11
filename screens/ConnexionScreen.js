1868e2decf39f799b174c9a97d0e7752d7b0aa54
matchls <mathieu.chales@gmail.com>
2026-03-11 17:49:48 +0100

    Revert "[CONNEXIONSCREEN] - Création modals pour se connecter"
    This reverts commit 2ec7cd20e4177262c541107a3fde1686fab2db71.

--- screens/ConnexionScreen.js
+++ screens/ConnexionScreen.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import * as React from "react";
 import {
   ActivityIndicator,
   KeyboardAvoidingView,
@@ -7,13 +8,12 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  View,
+  Alert,
 } from "react-native";
 import { useDispatch } from "react-redux";
 import { useUser, useSignUp, useSignIn } from "@clerk/clerk-expo";
 import Header from "../components/common/Header";
-import SignInModal from "../components/SignInModal";
-import SignUpModal from "../components/SignUpModal";
-import { useUser } from "@clerk/clerk-expo";
 
 export default function ConnexionScreen({ navigation }) {
   const dispatch = useDispatch();
@@ -26,49 +26,143 @@ export default function ConnexionScreen({ navigation }) {
     }
   }, [isSignedIn]);
 
+  const [loading, setLoading] = useState(false);
+  // *** Set SignUp function ************************************
+  const {
+    isLoaded: signUpLoaded,
+    signUp,
+    setActive: setActiveSignUp,
+  } = useSignUp();
+  const [signUpEmail, setSignUpEmail] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
+  // HANDLE SIGNUP PRESS
+  const onSignUpPress = async () => {
+    if (!signUpLoaded) return;
+
+    try {
+      setLoading(true);
+
+      const result = await signUp.create({
+        emailAddress: signUpEmail,
+        password: signUpPassword,
+      });
+      console.log("RESULTAAAAAT", result);
+
+      await setActiveSignUp({ session: result.createdSessionId });
+
+      navigation.replace("Creation");
+    } catch (err) {
+      Alert.alert("Sign Up Error", err.errors?.[0]?.message || "Try again");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // *** Set SignIn function ************************************
+  const {
+    isLoaded: signInLoaded,
+    signIn,
+    setActive: setActiveSignIn,
+  } = useSignIn();
+  const [signInEmail, setSignInEmail] = useState("");
+  const [signInPassword, setSignInPassword] = useState("");
+
+  // HANDLE SIGNIN PRESS
+  const onSignInPress = async () => {
+    if (!signInLoaded) return;
+
+    try {
+      setLoading(true);
+
+      const result = await signIn.create({
+        identifier: signInEmail,
+        password: signInPassword,
+      });
+
+      await setActiveSignIn({ session: result.createdSessionId });
+
+      navigation.replace("TabNavigator");
+    } catch (err) {
+      Alert.alert("Sign In Error", err.errors?.[0]?.message || "Try again");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (!signUpLoaded || !signInLoaded) {
+    return (
+      <View style={styles.loaderContainer}>
+        <ActivityIndicator size="large" color="#ec6e5b" />
+      </View>
+    );
+  }
+
   return (
-    <>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
       <Header
         title="ALTERAGO"
         navigation={navigation}
         showSettings={false}
         showVide={false}
       />
-      <KeyboardAvoidingView
-        style={styles.container}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-      >
-        <Text style={styles.title}>Welcome to AlterAgo</Text>
-
+      <Text style={styles.title}>Welcome to AlterAgo</Text>
+      {/* BLOC DE CONNEXION */}
+      <View style={styles.connexion}>
+        <TextInput
+          placeholder="Email"
+          onChangeText={(value) => setSignInEmail(value)}
+          value={signInEmail}
+          style={styles.input}
+          autoComplete={"email"}
+          inputMode={"email"}
+        />
+        <TextInput
+          placeholder="Password"
+          onChangeText={(value) => setSignInPassword(value)}
+          value={signInPassword}
+          style={styles.input}
+          autoComplete={"current-password"}
+          secureTextEntry={true}
+        />
         <TouchableOpacity
-          style={styles.mainButton}
-          onPress={() => setSignInVisible(true)}
+          onPress={() => onSignInPress()}
+          style={styles.button}
+          activeOpacity={0.8}
         >
           <Text style={styles.textButton}>Sign In</Text>
         </TouchableOpacity>
-
+      </View>
+      <View style={styles.line}></View>
+      <View style={styles.inscription}>
+        {/* BLOC DE CREATION DE COMPTE */}
+        <TextInput
+          placeholder="Email"
+          onChangeText={(value) => setSignUpEmail(value)}
+          value={signUpEmail}
+          style={styles.input}
+          autoComplete={"email"}
+          inputMode={"email"}
+        />
+        <TextInput
+          placeholder="Password"
+          onChangeText={(value) => setSignUpPassword(value)}
+          value={signUpPassword}
+          style={styles.input}
+          autoComplete={"new-password"}
+          secureTextEntry={true}
+        />
         <TouchableOpacity
-          style={styles.mainButton}
-          onPress={() => setSignUpVisible(true)}
+          onPress={() => onSignUpPress()}
+          style={styles.button}
+          activeOpacity={0.8}
         >
           <Text style={styles.textButton}>Sign Up</Text>
         </TouchableOpacity>
-
-        {/* Sign In Modal */}
-        <SignInModal
-          visible={signInVisible}
-          onClose={() => setSignInVisible(false)}
-          onSuccess={() => navigation.replace("TabNavigator")}
-        />
-
-        {/* Sign Up Modal */}
-        <SignUpModal
-          visible={signUpVisible}
-          onClose={() => setSignUpVisible(false)}
-          onSuccess={() => navigation.replace("Creation")}
-        />
-      </KeyboardAvoidingView>
-    </>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
