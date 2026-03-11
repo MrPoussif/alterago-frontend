569d05d6a6d2931b88f61f36a81c1760c42b2ec7
matchls <mathieu.chales@gmail.com>
2026-03-11 17:47:10 +0100

    small changes

--- screens/ConnexionScreen.js
+++ screens/ConnexionScreen.js
@@ -11,13 +11,9 @@ import {
 import { useDispatch } from "react-redux";
 import { useUser, useSignUp, useSignIn } from "@clerk/clerk-expo";
 import Header from "../components/common/Header";
-<<<<<<< HEAD
-import BottomSheet from "@gorhom/bottom-sheet";
-=======
 import SignInModal from "../components/SignInModal";
 import SignUpModal from "../components/SignUpModal";
 import { useUser } from "@clerk/clerk-expo";
->>>>>>> parent of b644226 ([CONNEXIONSCREEN] - Création modals pour se connecter 2)
 
 export default function ConnexionScreen({ navigation }) {
   const dispatch = useDispatch();
